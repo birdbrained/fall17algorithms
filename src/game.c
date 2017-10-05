a67575071b048f437ea29607e7e2acb5a48656c3
Matthew Kataryniak <mwk9@njit.edu>
2017-10-05 15:02:05 -0400

    Added changes to game.c

--- src/game.c
+++ src/game.c
@@ -5,6 +5,7 @@
 #include "ds_linked_list.h"
 #include "ds_priority_queue.h"
 #include "linkedlist.h"
+#include "dj_tilemap.h"
 
 
 int main(int argc, char * argv[])
@@ -13,6 +14,8 @@ int main(int argc, char * argv[])
     int done = 0;
     const Uint8 * keys;
 	Sprite *sprite;
+	TileMap *map;
+	static Vector2D path[2];
     
     int mx,my;
     float mf = 0;
@@ -37,6 +40,10 @@ int main(int argc, char * argv[])
     /*demo setup*/
     sprite = gf2d_sprite_load_image("images/backgrounds/bg_flat.png");
     mouse = gf2d_sprite_load_all("images/pointer.png",32,32,16);
+
+	map = tilemap_load("levels/tilemap.map");
+	vector2d_copy(path[0], map->start);
+	vector2d_copy(path[1], map->end);
 
 	//my stuff
 	/*pq = pq_new(sizeof(int), 10);
@@ -64,20 +71,23 @@ int main(int argc, char * argv[])
         
         gf2d_graphics_clear_screen();// clears drawing buffers
         // all drawing should happen betweem clear_screen and next_frame
-            //backgrounds drawn first
-            gf2d_sprite_draw_image(sprite,vector2d(0,0));
-            
-            //UI elements last
-            gf2d_sprite_draw(
-                mouse,
-                vector2d(mx,my),
-                NULL,
-                NULL,
-                NULL,
-                NULL,
-                &mouseColor,
-                (int)mf);
-        gf2d_grahics_next_frame();// render current draw frame and skip to the next frame
+		//backgrounds drawn first
+		gf2d_sprite_draw_image(sprite,vector2d(0,0));
+
+		tilemap_draw(map, vector2d(86, 24));
+		tilemap_draw_path(path, 2, map, vector2d(86, 24));
+
+		//UI elements last
+		gf2d_sprite_draw(
+			mouse,
+			vector2d(mx,my),
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			&mouseColor,
+			(int)mf);
+		gf2d_grahics_next_frame();// render current draw frame and skip to the next frame
         
         if (keys[SDL_SCANCODE_ESCAPE])done = 1; // exit condition
         //slog("Rendering at %f FPS",gf2d_graphics_get_frames_per_second());
