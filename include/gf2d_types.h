c3b51352fc0a84315104f520df9116211073b0fb
Matthew Kataryniak <mwk9@njit.edu>
2017-10-26 15:28:59 -0400

    Initial push of dj's branch

--- include/gf2d_types.h
+++ include/gf2d_types.h
@@ -1,13 +1,16 @@
 #ifndef __GF2D_TYPES_H__
 #define __GF2D_TYPES_H__
 
+#include <stdlib.h>
+
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
 #define GF2D_EPSILON   1e-6f
-
+#define GF2D_ROOT2  1.414213562
 #define GF2D_2PI 6.283185308
-
+#define GF2D_PI 3.141592654
+#define GF2D_HALF_PI 1.570796327
 /* conversion factor for converting from radians to degrees*/
 #define GF2D_RADTODEG  57.295779513082
 
@@ -24,22 +27,22 @@ typedef short unsigned int Bool;
 #endif
 
 /**
- * @brief random macro taken from Id Software's Quake 2 Source.
- * This macro exposes the more random bits from the rand() function
- * @return a random float between 0 and 1.0
- */
+* @brief random macro taken from Id Software's Quake 2 Source.
+* This macro exposes the more random bits from the rand() function
+* @return a random float between 0 and 1.0
+*/
 #define gf2d_random()  ((rand ()%1000)/(float)1000.0)
 
 /**
- * @brief random macro taken from Id Software's Quake 2 Source.
- * This macro exposes the more random bits from the rand() function
- * @return a random float between -1.0 and 1.0
- */
+* @brief random macro taken from Id Software's Quake 2 Source.
+* This macro exposes the more random bits from the rand() function
+* @return a random float between -1.0 and 1.0
+*/
 #define gf2d_crandom() (((float)((rand()%1000)/(float)1000.0) * 2.0) - 1.0)
 
 /**
- * basic operations
- */
+* basic operations
+*/
 #ifndef MIN
 #define MIN(a,b)          (a<=b?a:b)
 #endif
--- src/game.c
+++ src/game.c
@@ -1,137 +1,134 @@
 #include <SDL.h>
+#include <stdio.h>
 #include "gf2d_graphics.h"
 #include "gf2d_sprite.h"
 #include "simple_logger.h"
-#include "ds_linked_list.h"
-#include "ds_priority_queue.h"
-#include "ds_graph.h"
-#include "linkedlist.h"
-#include "dj_tilemap.h"
-
+#include "gf2d_draw.h"
+#include "gf2d_collision.h"
 
 int main(int argc, char * argv[])
 {
-    /*variable declarations*/
-    int done = 0;
-    const Uint8 * keys;
-	Sprite *sprite;
-	TileMap *map;
-	static Vector2D path[500];
-    
-    int mx,my;
-    float mf = 0;
-    Sprite *mouse;
-    Vector4D mouseColor = {255,100,255,200};
+	/*variable declarations*/
+	int done = 0;
+	const Uint8 * keys;
+	Sprite *sprite = NULL;
 
-	//Assignment variables
-	Graph *myGraph = graph_init(4, sizeof(int));
-	//Graph *testAStar = graph_init(4, sizeof(int));
-	GraphNode *start, *goal, *data;
-	PriorityQueue * endingTrail_head = pq_new(sizeof(GraphNode) + sizeof(int));
-	PriorityQueue * endingTrail_tail = pq_new(sizeof(GraphNode) + sizeof(int));
-	int i = 0;
-    
-    /*program initializtion*/
-    init_logger("gf2d.log");
-    slog("---==== BEGIN ====---");
-    gf2d_graphics_initialize(
-        "gf2d",
-        1200,
-        720,
-        1200,
-        720,
-        vector4d(0,0,0,255),
-        0);
-    gf2d_graphics_set_frame_delay(16);
-    gf2d_sprite_init(1024);
-    SDL_ShowCursor(SDL_DISABLE);
+	int mx, my, i;
+	float mf = 0;
+	Sprite *mouse = NULL;
+	Vector4D mouseColor = { 255,255,100,200 };
+	Space *space = NULL;
+	static Body body[10000];// not a pointer!
+	Shape shape[4];// not a pointer!
+				   /*program initializtion*/
+	init_logger("gf2d.log");
+	slog("---==== BEGIN ====---");
+	gf2d_graphics_initialize(
+		"gf2d",
+		1200,
+		720,
+		1200,
+		720,
+		vector4d(0, 0, 0, 255),
+		0);
+	gf2d_graphics_set_frame_delay(16);
+	gf2d_sprite_init(1024);
+	SDL_ShowCursor(SDL_DISABLE);
 
-    /*demo setup*/
-    sprite = gf2d_sprite_load_image("images/backgrounds/bg_flat.png");
-    mouse = gf2d_sprite_load_all("images/pointer.png",32,32,16);
+	/*demo setup*/
+	sprite = gf2d_sprite_load_image("images/backgrounds/bg_flat.png");
+	mouse = gf2d_sprite_load_all("images/pointer.png", 32, 32, 16);
 
-	map = tilemap_load("levels/tilemap.map");
-	//vector2d_copy(path[0], map->start);
-	//vector2d_copy(path[1], vector2d(10, 10));
-	//vector2d_copy(path[2], map->end);
+	space = gf2d_space_new_full(
+		3,
+		gf2d_rect(0, 0, 1200, 700),
+		0.1,
+		vector2d(0, 0.1),
+		1,
+		0.1);
 
-	//my stuff for assignment
-	/*if (myGraph != NULL)
+	shape[0] = gf2d_shape_circle(0, 0, 5);
+	shape[1] = gf2d_shape_circle(20, 0, 25);
+	shape[2] = gf2d_shape_rect(-32, -32, 64, 64);
+	shape[3] = gf2d_shape_rect(-16, -16, 32, 32);
+
+	//	gf2d_space_add_static_shape(space,gf2d_shape_rect(200,500, 512,32));
+	/* Stress test*/
+	for (i = 0; i < 100;i++)
 	{
-		for (i = 0; i < 16; i++)
-		{
-			graph_insert(myGraph, i, myGraph->width, sizeof(int));
-		}
-		graph_print(myGraph);
-		//slog("down data from start (%i)", myGraph->head->down_node->data);
-		//slog("width: (%i)", myGraph->width);
-		graph_print_squiggle(myGraph, 7);
-	}*/
-	if (myGraph != NULL)
-	{
-		myGraph = graph_load_from_tilemap(map, sizeof(int), 1);
-		start = graph_find_node(myGraph, map->start.x, map->start.y);
-		goal = graph_find_node(myGraph, map->end.x, map->end.y);
-		if (start != NULL && goal != NULL)
-		{
-			slog("reach the end? %i", graph_a_star(&start, goal, sizeof(int), endingTrail_head, endingTrail_tail));
-		}
-		//graph_print(myGraph);
+		gf2d_body_set(
+			&body[i],
+			"body",
+			ALL_LAYERS,
+			0,
+			vector2d(
+				600 + (gf2d_crandom() * 500),
+				360 + (gf2d_crandom() * 300)),
+			vector2d(gf2d_crandom(), gf2d_crandom()),
+			10,
+			1,
+			1,
+			&shape[i%4],
+			NULL,
+			NULL,
+			NULL);
+		gf2d_space_add_body(space, &body[i]);
 	}
-	while (endingTrail_tail != NULL && endingTrail_tail->data != NULL)
+	/* collision test*/
+	//         gf2d_body_set(
+	//             &body[0],
+	//             "Body A",
+	//             ALL_LAYERS,
+	//             0,
+	//             vector2d(100,300),
+	//             vector2d(-1,0),
+	//             1000,
+	//             0,
+	//             0,
+	//             &shape[2],
+	//             NULL,
+	//             NULL,
+	//             NULL);
+	//          gf2d_space_add_body(space,&body[0]);
+	//         gf2d_body_set(
+	//             &body[1],
+	//             "Body B",
+	//             ALL_LAYERS,
+	//             0,
+	//             vector2d(600,340),
+	//             vector2d(1,1),
+	//             10,
+	//             1,
+	//             1,
+	//             &shape[0],
+	//             NULL,
+	//             NULL,
+	//             NULL);
+	//         gf2d_space_add_body(space,&body[1]);
+
+	//*/
+	/*main game loop*/
+	while (!done)
 	{
-		data = pq_delete(endingTrail_head, endingTrail_tail);
-		slog("final: x (%i), y (%i), data (%i)", data->x, data->y, data->data);
-	}
-	/*if (testAStar != NULL)
-	{
-		graph_insert(testAStar, 0, testAStar->width, sizeof(int));
-		graph_insert(testAStar, 0, testAStar->width, sizeof(int));
-		graph_insert(testAStar, 0, testAStar->width, sizeof(int));
-		graph_insert(testAStar, 0, testAStar->width, sizeof(int));
-		graph_insert(testAStar, 1, testAStar->width, sizeof(int));
-		graph_insert(testAStar, 1, testAStar->width, sizeof(int));
-		graph_insert(testAStar, 0, testAStar->width, sizeof(int));
-		graph_insert(testAStar, 0, testAStar->width, sizeof(int));
-		graph_insert(testAStar, 0, testAStar->width, sizeof(int));
-		graph_insert(testAStar, 0, testAStar->width, sizeof(int));
-		graph_insert(testAStar, 0, testAStar->width, sizeof(int));
-		graph_insert(testAStar, 1, testAStar->width, sizeof(int));
-		graph_insert(testAStar, 0, testAStar->width, sizeof(int));
-		graph_insert(testAStar, 1, testAStar->width, sizeof(int));
-		graph_insert(testAStar, 0, testAStar->width, sizeof(int));
-		graph_insert(testAStar, 0, testAStar->width, sizeof(int));
-		start = graph_find_node(testAStar, 0, 0);
-		goal = graph_find_node(testAStar, 0, 3);
-		if (start != NULL && goal != NULL)
-		{
-			slog("reach the end? %i", graph_a_star(&start, goal, sizeof(int)));
-		}
-	}*/
+		SDL_PumpEvents();   // update SDL's internal event structures
+		keys = SDL_GetKeyboardState(NULL); // get the keyboard state for this frame
+										   /*update things here*/
+		SDL_GetMouseState(&mx, &my);
+		mf += 0.1;
+		if (mf >= 16.0)mf = 0;
 
-    /*main game loop*/
-    while(!done)
-    {
-        SDL_PumpEvents();   // update SDL's internal event structures
-        keys = SDL_GetKeyboardState(NULL); // get the keyboard state for this frame
-        /*update things here*/
-        SDL_GetMouseState(&mx,&my);
-        mf+=0.1;
-        if (mf >= 16.0)mf = 0;
-        
-        
-        gf2d_graphics_clear_screen();// clears drawing buffers
-        // all drawing should happen betweem clear_screen and next_frame
-		//backgrounds drawn first
-		gf2d_sprite_draw_image(sprite,vector2d(0,0));
 
-		tilemap_draw(map, vector2d(86, 24));
-		tilemap_draw_path(path, 3, map, vector2d(86, 24));
+		gf2d_graphics_clear_screen();// clears drawing buffers
+									 // all drawing should happen betweem clear_screen and next_frame
+									 //backgrounds drawn first
+		gf2d_sprite_draw_image(sprite, vector2d(0, 0));
+		gf2d_space_update(space);
 
+		gf2d_space_draw(space);
 		//UI elements last
 		gf2d_sprite_draw(
 			mouse,
-			vector2d(mx,my),
+			vector2d(mx, my),
 			NULL,
 			NULL,
 			NULL,
@@ -139,11 +136,13 @@ int main(int argc, char * argv[])
 			&mouseColor,
 			(int)mf);
 		gf2d_grahics_next_frame();// render current draw frame and skip to the next frame
-        
-        if (keys[SDL_SCANCODE_ESCAPE])done = 1; // exit condition
-        //slog("Rendering at %f FPS",gf2d_graphics_get_frames_per_second());
-    }
-    slog("---==== END ====---");
-    return 0;
+
+		if (keys[SDL_SCANCODE_ESCAPE])done = 1; // exit condition
+		slog("Rendering at %f FPS", gf2d_graphics_get_frames_per_second());
+	}
+
+	gf2d_space_free(space);
+	slog("---==== END ====---");
+	return 0;
 }
 /*eol@eof*/
